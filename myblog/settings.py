0a9dfc62d91d940f3832d458e5cd9cb2cc7a2c9f
whduddhks <39693818+whduddhks@users.noreply.github.com>
2019-05-23 19:58:35 +0900

    Host allowed

--- myblog/settings.py
+++ myblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')gtxg)nzc8(w#z*z1x_r#&enj1=+1r
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
